2fac721caf5de1f550f2a3632cdeef3612123d94
Yitao Yang <www.speedway@hotmail.com>
2018-03-04 22:16:59 -0500

    rr
    in progress

--- main.cpp
+++ main.cpp
@@ -83,6 +83,92 @@ void read_p(std::vector<Process> &p, std::ifstream &input){
         }
     }
 }
+/*
+ should print:
+ avg cpu burst time
+ avg wait time
+ #context swtices
+ #preemtions
+*/
+void round_robin(std::vector<Process> p){
+    std::vector<Process> p_s = p;
+    std::vector<Process> queue;
+    std::vector<Process> finished;
+    Process cpu; //the process using the cpu
+    std::vector<Process> blocked;
+    bool b_finish = 0;
+    int process_count = p.size();
+    int cb_count = 0; //cpu burst count
+    int preemp = 0;
+    int c_sw = 0; //count of context switch
+    int t_slice = 80;
+    int t_cs = 8;
+    int time = -1; //time
+    int cs_cd = 0; //context switch countdown
+    bool cpu_in_use = false;
+    bool c_swing = false; //cw status
+    //while both queue and processes-on-the-way are not empty
+    //(while there are unprocessed processes)
+    while(p_s.size() != 0 && queue.size() != 0){
+        //check if context switch is done, if yes,
+        //change context switch status and reset countdown
+        if(cs_cd == t_cs){
+            cs_cd = 0;
+            c_swing = false;
+        }
+        //add wait time of all processes in queue by 1
+        if(time != -1){
+            for(int a = 0; a < queue.size(); a++){
+                queue[a].wait_();
+            }
+        }
+        time += 1;
+        //check if process(es) arrive
+        (for int a = 0; a < p.size; a++){
+            if(p[a].arrial_t == t){
+                //add process t queue
+                queue.push_back(p[a]);
+                p.erase(p.begin()+a);
+                //minus one because size of p decrease by 1
+                a--;
+            }
+        }
+        //now newly arrived process(es) are added to queue (if any)
+        //push the first one in queue into CPU...?
+        //check if cpu is in use first
+        //if cpu is in use
+        if(cpu_in_use){
+            b_finish = cpu.run_cpu_burst(1);
+        }
+        //if cpu is not in use and context switch not taking place
+        else if(!cpu_in_use && !c_swing){
+            cpu_in_use = true;
+            //push the first process in queue into cpu
+            cpu = queue[0];
+            //remove the process from queue
+            queue.erase(queue.begin());
+            //what else to do...?
+        }
+        //if a process has done cpu bursting...?
+        if(cpu_in_use && b_finish){
+            //the process is now blocked on io
+            cpu.io_start();
+            //move the process to blocked list
+            blocked.push_back(cpu);
+            //context switch begins
+            c_swing = true;
+            //cpu not in use
+            cpu_in_use = false;
+        }
+        //if context switch is taking place
+        if(c_swing){
+            cs_cd += 1;
+        }
+    }
+}
+
+//looks like processes are sorted by arrival time and process id - great!
+
 int main(int argc, char* argv[]){
     std::vector<Process> p;
     std::string input_file = argv[1];
--- process.cpp
+++ process.cpp
@@ -15,6 +15,11 @@ private:
     int cpu_burtst_time;
     int num_bursts;
     int io_time;
+    int wait_time;
+    int cbt_remain;
+    int io_remain;
+    bool io_ing;
+    //bool done;
     /*
     int remaining_bursts;
     int remaining_burst_time;
@@ -28,11 +33,67 @@ public:
         cpu_burtst_time = b_time;
         num_bursts = num_b;
         io_time = io_t;
+        cbt_remain = cpu_burtst_time;
+        io_remain = io_time;
+        wait_time = 0;
+        io_ing = false;
+        //done = false;
     }
     //test
     void Printp(){
         std::cout << process_id <<"|"<< arrival_time << "|" << cpu_burtst_time << "|" << num_bursts
         << "|" << io_time << std::endl;
+    }
+    bool check_done(){
+        return (num_bursts == 0);
+    }
+    int arrival_t(){
+        return arrival_time;
+    }
+    int cbt(){
+        return cpu_burtst_time;
+    }
+    int num_b(){
+        return num_bursts;
+    }
+    int iot(){
+        return io_time;
+    }
+    int wait_t(){
+        return wait_time;
+    }
+    void wait_(){
+        wait_time += 1;
+    }
+    void io_start(){
+        io_ing = true;
+    }
+    void io_end(){
+        io_ing  = false;
+    }
+    void io_(){
+        io += 1;
+    }
+    char p_id(){
+        return process_id;
+    }
+    //run cpu burst for the time given (a)
+    //hopefully a would be 1
+    //return t if a burst is completed
+    //return f otherwise
+    bool run_cpu_burst(int a){
+        //if time given is smaller than or equal to the remaining time of current burst
+        //num_bursts - 1;
+        if(cbt_remain <= a){
+            num_bursts -= 1;
+            cbt_remain = cpu_burtst_time;
+            return true;
+        }
+        else{
+            cbt_remain -= a;
+            return false;
+        }
+        return false;
     }
     
 };
